48eb5c3d10f1af7fa27746ae6ee97725feffcc0a
DumiTech <cernei_dumitru@protonmail.com>
2023-01-28 02:16:58 +0200

    imob scraping refactorization

--- imobiliare/imobiliare/spiders/imobiliare_spider.py
+++ imobiliare/imobiliare/spiders/imobiliare_spider.py
@@ -1,48 +1,54 @@
+import time
 from pprint import pprint
-from scrapy.exporters import CsvItemExporter
 
 import scrapy
+from csv import writer
 
 
 class QuotesSpider(scrapy.Spider):
     name = "im_spider"
+    file_name = f"{time.time()}_output.csv"
 
     start_urls = [
         'https://www.imobiliare.ro/vanzare-apartamente/bucuresti?pagina=1/',
     ]
 
-    # define the fields that will be exported to the CSV file
-    fields_to_export = ['title', 'type', 'location', 'rooms', 'square_meters', 'floor', 'setup', 'price', 'price_extension']
-
-    def __init__(self):
-        self.csvfile = open("output.csv", 'w')
-        self.exporter = CsvItemExporter(self.csvfile, fields_to_export=self.fields_to_export)
-        self.exporter.start_exporting()
 
     def parse(self, response):
         fail_count = 0
-        for quote in response.css('div.col_descriere'):
-            try:
-                item = {
-                    'title': quote.css('h2.titlu-anunt span::text')[1].getall(),
-                    'type': quote.css('h2.titlu-anunt span::text')[0].getall(),
-                    'location': quote.css('p.location_txt span::text').get(),
-                    'rooms': quote.css('div.caracteristica span span strong::text')[0].getall(),
-                    'square_meters': quote.css('div.caracteristica span span strong::text')[1].getall(),
-                    'floor': quote.css('div.caracteristica span span strong::text')[2].getall(),
-                    'setup': quote.css('div.caracteristica span span strong::text')[3].getall(),
-                    'price': quote.css('span.pret-mare::text').getall(),
-                    'price_extension': quote.css('span.tva-luna::text').getall(),
-                }
-                item['location'] = item['location'].strip()
-                pprint(item)
-                self.exporter.export_item(item)
-            except:
-                fail_count += 1
-                print("+ 1 fail..")
+        with open('imobiliare/reports/{file_name}_imobiliare.csv'.format(file_name=self.file_name), 'a',
+                  encoding='utf-8', newline='') as f:
+            thewriter = writer(f)
+            header = ["Title", "Location", "Rooms", "Square meters", "Floor", "Setup", "Price [K]", "Currency & VAT"]
+            thewriter.writerow(header)
+
+            for quote in response.css('div.col_descriere'):
+                try:
+                    title = quote.css('h2.titlu-anunt span::text')[1].extract()
+                    location = quote.css('p.location_txt span::text').get()
+                    rooms = quote.css('div.caracteristica span span strong::text')[0].extract()
+                    square_meters = quote.css('div.caracteristica span span strong::text')[1].get()
+                    floor = quote.css('div.caracteristica span span strong::text')[2].extract()
+                    setup = quote.css('div.caracteristica span span strong::text')[3].extract()
+                    price = quote.css('span.pret-mare::text').get()
+                    price_extension = quote.css('span.tva-luna::text').get()
+
+                    title = title.strip()
+                    location = location.strip()
+
+                    if "TVA" in price_extension:
+                        price_extension = "E + TVA"
+                    else:
+                        price_extension = "E"
+
+                    if "o" in rooms:
+                        rooms = "1"
+
+                    fetched_data = [title, location, rooms, square_meters, floor, setup, price, price_extension]
+                    pprint(fetched_data)
+                    thewriter.writerow(fetched_data)
+                except:
+                    fail_count += 1
+                    print("+ 1 fail..")
 
         print("Number of fails: ", fail_count)
-
-    def closed(self, reason):
-        self.exporter.finish_exporting()
-        self.csvfile.close()
